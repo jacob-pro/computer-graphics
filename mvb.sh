1d4925a0ef36d6710e81510eeb402c25b1ea1fc7
Jacob Halsey <jacob@jhalsey.com>
2020-11-27 23:37:15 +0000

    run on mvb

---
+++ mvb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+## We need C++ 17 support, the lab machines are outdated
+## We don't have root access
+## So download and built GCC 10 from source
+wget -O gcc.tar.gz ftp://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-10.1.0/gcc-10.1.0.tar.gz
+tar -xzf gcc.tar.gz
+rm gcc.tar.gz ## Save some space!
+cd gcc-10.1.0
+./contrib/download_prerequisites
+cd ..
+mkdir objdir
+cd objdir
+$PWD/../gcc-10.1.0/configure --prefix=$HOME/GCC-10.1.0 --enable-languages=c++
+make -j 12  ## i7-8700 = 6 cores
+make install
+cd ../
+
+## Compile CG2020
+cmake -DCMAKE_C_COMPILER="$HOME/GCC-10.1.0/bin/gcc" -DCMAKE_CXX_COMPILER="$HOME/GCC-10.1.0/bin/g++" .
+make
+## We need to use the new libstdc++
+export LD_LIBRARY_PATH="$HOME/GCC-10.1.0/lib64":$LD_LIBRARY_PATH
+./CG2020
