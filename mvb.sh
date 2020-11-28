17337cf97553e3e64cffd774a77b74a627f8a518
Jacob Halsey <jacob@jhalsey.com>
2020-11-28 00:28:22 +0000

    improve script

--- mvb.sh
+++ mvb.sh
@@ -5,6 +5,7 @@ set -e
 ## We need C++ 17 support, the lab machines are outdated
 ## We don't have root access
 ## So download and built GCC 10 from source
+## This will take a long time!
 if [ ! -d $HOME/GCC-10.1.0 ]; then
 
   if [ ! -d gcc-10.1.0 ]; then
@@ -28,7 +29,7 @@ if [ ! -d $HOME/GCC-10.1.0 ]; then
 fi
 
 ## Compile CG2020
-cmake -DCMAKE_C_COMPILER="$HOME/GCC-10.1.0/bin/gcc" -DCMAKE_CXX_COMPILER="$HOME/GCC-10.1.0/bin/g++" .
+cmake -DCMAKE_C_COMPILER="$HOME/GCC-10.1.0/bin/gcc" -DCMAKE_CXX_COMPILER="$HOME/GCC-10.1.0/bin/g++" -DCMAKE_BUILD_TYPE=Release .
 make
 ## We need to use the new libstdc++
 export LD_LIBRARY_PATH="$HOME/GCC-10.1.0/lib64":$LD_LIBRARY_PATH
